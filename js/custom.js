76648ab733c2e1916bbb27d123415340c156b304
Btngan2208 <nganbt.longan@gmail.com>
2024-12-01 05:27:15 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+               	strings: ["WELCOME", "XIN CHÀO"],
+       	 	typeSpeed:50,
+        		backSpeed:50,
+        		loop:true,
+        		});
+
+$('#banner').owlCarousel({
+   	 loop:true,	// cho lap lại
+	items:1,	// xuất hiện 1 ảnh thôi
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+    	autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+nav:true,	// cho xuất hiện bộ nút tới lui 
+	  })
+
+
+
+
+
+
+
